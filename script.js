5773319c94b00140a805f63c2281539b756c312f
Tai_TN <zztainguyen1311@gmail.com>
2026-04-04 18:12:22 +0700

    code ccjnx vibe thoi

--- script.js
+++ script.js
@@ -1,22 +1,49 @@
-// Tóm lấy 2 phần tử cần thiết
-const nutTacKe = document.getElementById("nut-tac-ke-hoa");
-const dongChu = document.getElementById("trang-thai");
+const danhSachDienThoai = [
+    {
+        id: 1,
+        ten: "iPhone 15 Pro Max",
+        gia: "29.990.000₫",
+        hinh: "https://cdn.tgdd.vn/Products/Images/42/305658/iphone-15-pro-max-blue-thumbnew-600x600.jpg"
+    },
+    {
+        id: 2,
+        ten: "Samsung Galaxy S24 Ultra",
+        gia: "31.990.000₫",
+        hinh: "https://tse1.mm.bing.net/th/id/OIP.FpxLz2z3K0uTLDDu0ZNBMgHaHc?rs=1&pid=ImgDetMain&o=7&rm=3"
+    },
+    {
+        id: 2,
+        ten: "Samsung Galaxy S24 Ultra",
+        gia: "31.990.000₫",
+        hinh: "https://tse1.mm.bing.net/th/id/OIP.FpxLz2z3K0uTLDDu0ZNBMgHaHc?rs=1&pid=ImgDetMain&o=7&rm=3"
+    },{
+        id: 2,
+        ten: "Samsung Galaxy S24 Ultra",
+        gia: "31.990.000₫",
+        hinh: "https://tse1.mm.bing.net/th/id/OIP.FpxLz2z3K0uTLDDu0ZNBMgHaHc?rs=1&pid=ImgDetMain&o=7&rm=3"
+    },{
+        id: 2,
+        ten: "Samsung Galaxy S24 Ultra",
+        gia: "31.990.000₫",
+        hinh: "https://tse1.mm.bing.net/th/id/OIP.FpxLz2z3K0uTLDDu0ZNBMgHaHc?rs=1&pid=ImgDetMain&o=7&rm=3"
+    },
+];
 
-// Sự kiện 1: Khi LƯỚT CHUỘT VÀO
-nutTacKe.addEventListener("mouseenter", function() {
-    nutTacKe.style.backgroundColor = "orange";
+const khoSanPham = document.getElementById("danh-sach-sp");
+
+danhSachDienThoai.forEach(function(item){
+    const htmlCard = `
+    <div class="the-san-pham">
+    <img src="${item.hinh}" alt="${item.ten}">
+    <h3>${item.ten}</h3>
+    <p class="gia-tien">${item.gia}</p>
+        <div class="nhom-nut-mua">
+            <button class="nut-them-vao-gio">Thêm vào giỏ</button>
+            <button class="nut-mua">Mua hàng</button>
+            </div>
+    </div>
+    `
+
+    khoSanPham.insertAdjacentHTML("beforeend", htmlCard);
 });
 
-// Sự kiện 2: Khi RỜI CHUỘT RA
-nutTacKe.addEventListener("mouseleave", function() {
-    nutTacKe.style.backgroundColor = "blue"; // Trả lại màu gốc
-});
-
-// Sự kiện 3: Khi CLICK CHUỘT
-nutTacKe.addEventListener("click", function() {
-    // Đổi chữ của thẻ h2 ở trên
-    dongChu.innerText = "Wow! Bạn vừa bấm trúng con tắc kè!";
-    
-    // Đổi luôn chữ của chính cái nút
-    nutTacKe.innerText = "Đã bị bắt!";
-});
