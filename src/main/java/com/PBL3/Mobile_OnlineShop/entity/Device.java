24b35e38360e6a3e389e83eb8a87ebf943fd342d
huyhoangkhdl <dcmm987654321@gmail.com>
2026-04-17 08:00:52 +0700

    create entity

---
+++ src/main/java/com/PBL3/Mobile_OnlineShop/entity/Device.java
@@ -0,0 +1,29 @@
+package com.PBL3.Mobile_OnlineShop.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import jakarta.validation.constraints.Pattern;
+
+@Entity
+@Table(name = "device")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Device {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long deviceId;
+
+    @NotBlank(message = "IMEI không được để trống")
+    @Pattern(regexp = "^[0-9]{15}$", message = "IMEI phải bao gồm đúng 15 chữ số")
+    @Column(length = 50, nullable = false, unique = true)
+    private String imei;
+
+    @ManyToOne
+    @JoinColumn(name = "product_variant_id")
+    private ProductVariant productVariant;
+
+    @Column(length = 20, name = "status")
+    private String status;
+}
---
+++ src/main/java/com/PBL3/Mobile_OnlineShop/entity/User.java
@@ -0,0 +1,55 @@
+package com.PBL3.Mobile_OnlineShop.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import java.util.List;
+import com.PBL3.Mobile_OnlineShop.enums.Role;
+
+@Entity
+@Table(name = "`user`")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @NotBlank(message = "Tên đăng nhập không được để trống")
+    @Size(min = 3, max = 100, message = "Tên đăng nhập phải từ 3 đến 100 ký tự")
+    @Column(length = 100, nullable = false, unique = true)
+    private String username;
+
+    @NotBlank(message = "Email không được để trống")
+    @Email(message = "Email không đúng định dạng")
+    @Column(length = 255, nullable = false, unique = true)
+    private String email;
+
+    @NotBlank(message = "Số điện thoại không được để trống")
+    @Pattern(regexp = "^(0|\\+84)[0-9]{9}$", message = "Số điện thoại không hợp lệ")
+    @Column(length = 20, nullable = false, unique = true)
+    private String phoneNumber;
+
+    @NotBlank(message = "Mật khẩu không được để trống")
+    @Size(min = 6, message = "Mật khẩu phải có ít nhất 6 ký tự")
+    @Column(length = 255, nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20, nullable = false)
+    private Role role;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private Cart cart;
+
+    @OneToMany(mappedBy = "user")
+    private List<Order> orders;
+
+    @OneToMany(mappedBy = "user")
+    private List<ProductReview> reviews;
+}
